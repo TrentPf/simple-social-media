e56169e1cf617ba804c5c571547949c80e0d83ea
Trent Pfeiffer <trentpf@hotmail.com>
2022-02-03 17:08:52 -0500

    Add user routes and more structure

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
+
+  get '/registration' => 'users#new'
+  post '/users' => 'users#create'
 end
